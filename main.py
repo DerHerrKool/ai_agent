8f1e01d44295f2d5363ce5762dac35737e9616df
DerHerrKool <derherrkool@gmail.com>
2026-01-18 06:56:11 +0100

    20260118 Token Metadata

--- main.py
+++ main.py
@@ -4,15 +4,29 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-if api_key == None:
+if api_key is None:
     raise RuntimeError ("There is no API key")
 
 from google import genai
 
 client = genai.Client(api_key=api_key)
 
+user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.5-flash", contents=user_prompt,
 )
+
+
+usage = response.usage_metadata
+if usage is None:
+    raise RuntimeError("There is no usage metadata")
+else:
+    prompt_tokens = usage.prompt_token_count
+    response_tokens = usage.candidates_token_count
+
+print(f"User prompt: {user_prompt}") 
+print(f"Prompt tokens: {prompt_tokens}")
+print(f"Response tokens: {response_tokens}")
 print(response.text)
 
